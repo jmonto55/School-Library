4fe407ebb511ac539c71bded4edae8ba16b9d831
jmonto55 <jmonto55@gmail.com>
2023-04-14 01:49:29 -0500

    Add changes required by code reviewer

--- person.rb
+++ person.rb
@@ -4,8 +4,8 @@ require_relative './trimmer_decorator'
 require_relative './rental'
 
 class Person < Nameable
-  attr_accessor :name, :age
-  attr_reader :id, :rentals
+  attr_accessor :name, :age, :rentals
+  attr_reader :id
 
   def initialize(age, name = 'unknown', parent_permission: true)
     @id = Random.rand(1..1000)
